e67972569eb1533b7034e88f924eddaf76f39503
Zofia Stawarska <z.stawarska@gmail.com>
2024-08-18 10:34:27 +0200

    create structure slices

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-import './store.js'
+import store from  './store.js'
+
+store.dispatch({ type: 'account/deposit', payload: 100 });
+console.log(store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
