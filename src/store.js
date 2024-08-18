817913e174716869c959de74566ab498dc75aea4
Zofia Stawarska <zstawarska@future-processing.com>
2024-08-18 09:47:03 +0200

    action creators

--- src/store.js
+++ src/store.js
@@ -10,12 +10,12 @@ function reducer(state = initialState, action) {
     switch(action.type) {
         case 'account/deposit':
             return {...state, balance: state.balance + action.payload}
-        case 'account/withdwar':
+        case 'account/withdraw':
             return {...state, balance: state.balance - action.payload}
         case 'account/requestLoan':
             if (state.loan > 0) return state
             return {...state, loan: action.payload.amount, loanPurpose: action.payload.purpose, balance: state.balance + action.payload.amount}
-        case 'account/pauLoan':
+        case 'account/payLoan':
             return {...state, loan: 0, loanPurpose: '', balance: state.balance - state.loan}
             default:
                 return state
@@ -23,3 +23,33 @@ function reducer(state = initialState, action) {
 }
 
 const store = createStore(reducer)
+
+function deposit(amount) {
+    return {
+        type: 'account/deposit',
+        payload: amount
+    }
+}
+
+function withdraw(amount) {
+    return {
+        type: 'account/withdraw',
+        payload: amount
+    }
+}
+
+function requestLoan(amount, purpose) {
+    return {
+        type: 'account/requestLoan',
+        payload: { 
+            amount, 
+            purpose 
+        }
+    }
+}
+
+function payLoan() {
+    return {
+        type: 'account/payLoan'
+    }
+}
